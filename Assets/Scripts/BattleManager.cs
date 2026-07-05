4694f55bc1d013c38f52dfd5e0fd9a12d68e2120
coaltic <glindsay156@gmail.com>
2026-07-05 16:59:27 -0300

    Fixed Enemy Spawning
    Enemies will now spawn on their own tiles

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,33 +1,38 @@
-using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BattleManager : MonoBehaviour
 {
     public GameObject[] enemyPrefabs;
-    public GameObject[] spawnLocations;
+    // public GameObject[] spawnLocations;
+    public List<GameObject> spawnLocations;
     public GameObject[] activeEnemies;
     public GameObject[] takenLocations;
     public int enemyIDNumber;
     public int enemySpawnNumber;
 
+    public int ranNum;
+
    
     void Awake()
     {
-        enemySpawnNumber = Random.Range(0, spawnLocations.Length);
+        enemySpawnNumber = Random.Range(0, spawnLocations.Count);
+        // enemySpawnNumber = Random.Range(0, spawnLocations.Length);
         enemyIDNumber = Random.Range(0, enemyPrefabs.Length);
-        activeEnemies[] = new GameObject[(enemySpawnNumber - 1);
-        
+        activeEnemies = new GameObject[(enemySpawnNumber)];
+        // activeEnemies[] = new GameObject[(enemySpawnNumber - 1);
+
 
         for (int i = 0; i < enemySpawnNumber; i++)
         {
             activeEnemies[i] = Instantiate(enemyPrefabs[0]);
 
-
-            activeEnemies[i].transform.position = spawnLocations[Random.Range(0, spawnLocations.Length)].transform.position;
+            ranNum = Random.Range(0, spawnLocations.Count);
+            activeEnemies[i].transform.position = spawnLocations[ranNum].transform.position;
+            SpawnLocationUpdater();
 
             //takenLocations[i] = spawnLocations
         }
-
 
         //GameObject enemy1 = Instantiate(enemyPrefabs[0]);
         //enemy1.transform.position = spawnLocations[Random.Range(0, spawnLocations.Length)].transform.position;
@@ -37,5 +42,20 @@ public class BattleManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    
+
+    void TileAvailabilityCheck()
+    {
+        int newRanNum = Random.Range(0, spawnLocations.Count);
+        
+        spawnLocations.Remove(spawnLocations[ranNum]);
+
+    }
+
+    void SpawnLocationUpdater()
+    {
+        spawnLocations.Remove(spawnLocations[ranNum]);
     }
 }
